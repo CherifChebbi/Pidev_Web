4fa2b381f90389f605fefc620909b384fdd827f1
majdzarai <109704608+majdzarai@users.noreply.github.com>
2024-03-04 16:05:31 +0100

    add comments

---
+++ src/Controller/CommentaireController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Commentaire;
+use App\Form\CommentaireType;
+use App\Repository\CommentaireRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/commentaire')]
+class CommentaireController extends AbstractController
+{
+    #[Route('/', name: 'app_commentaire_index', methods: ['GET'])]
+    public function index(CommentaireRepository $commentaireRepository): Response
+    {
+        return $this->render('commentaire/index.html.twig', [
+            'commentaires' => $commentaireRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_commentaire_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $commentaire = new Commentaire();
+        $form = $this->createForm(CommentaireType::class, $commentaire);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            
+
+
+            $entityManager->persist($commentaire);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_commentaire_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('commentaire/new.html.twig', [
+            'commentaire' => $commentaire,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_commentaire_show', methods: ['GET'])]
+    public function show(Commentaire $commentaire): Response
+    {
+        return $this->render('commentaire/show.html.twig', [
+            'commentaire' => $commentaire,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_commentaire_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Commentaire $commentaire, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(CommentaireType::class, $commentaire);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_commentaire_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('commentaire/edit.html.twig', [
+            'commentaire' => $commentaire,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_commentaire_delete', methods: ['POST'])]
+    public function delete(Request $request, Commentaire $commentaire, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$commentaire->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($commentaire);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_commentaire_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
